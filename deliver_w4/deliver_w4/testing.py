c89d6372a5f8d229b0757267fc1241003a3223f4
Yeiry Melendez <yymelendez@ucsd.edu>
2024-11-06 03:29:46 +0000

    modified turtle cleaner

--- deliver_w4/deliver_w4/testing.py
+++ deliver_w4/deliver_w4/testing.py
@@ -13,7 +13,7 @@ class TurtleBotVacuumCleaner(Node):
         super().__init__('turtlebot_vacuum_cleaner')
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
         self.subscriber_ = self.create_subscription(Odometry, '/odom', self.update_pose, 10)
-        self.timer = self.create_timer(0.1, self.clean_area)
+        self.timer = self.create_timer(0.5, self.clean_area)
         self.cmd = Twist()
 
         
@@ -21,11 +21,14 @@ class TurtleBotVacuumCleaner(Node):
         self.current_y = 0.0
         self.current_yaw = 0.0
 
-        
-        self.linear_velocity = 0.2  
+        self.cmd.linear.x = 0.0
+        self.cmd.angular.z = 0.0
+        self.publisher_.publish(self.cmd)
+
+        self.linear_velocity = 0.2
         self.angular_velocity = 0.5  
-        self.radius_increase_rate = 0.003  
-        self.max_radius = 10.0  
+        self.radius_increase_rate = 0.005  
+        self.max_radius = 6 
        
         self.stop_cleaning = False  
 
@@ -61,13 +64,11 @@ class TurtleBotVacuumCleaner(Node):
             return
 
         
-            self.linear_velocity += self.radius_increase_rate
-        else:
-            self.linear_velocity = self.max_radius
-
         
-        self.cmd.linear.x = self.linear_velocity
-        self.cmd.angular.z = self.angular_velocity  
+        else:
+            self.linear_velocity += self.radius_increase_rate
+            self.cmd.linear.x = self.linear_velocity
+            self.cmd.angular.z = self.angular_velocity  
 
         
         self.publisher_.publish(self.cmd)
